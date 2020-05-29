2deed9ef1444f186117b3c02589240dd85c358bf
boulderwhis <eleavit1@kent.edu>
2020-05-28 22:53:43 -0700

    shift to index.js start

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
+  .use(express.static(path.join('The-Democratic-Sun', 'public')))
+  .set('views', path.join('The-Democratic-Sun', 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-var PORT = process.env.PORT || 5000; 
-var express = require('express');
-var app = express();
+// var PORT = process.env.PORT || 5000; 
+// var express = require('express');
+// var app = express();
 
-var http = require('http');
-var server = http.Server(app);
+// var http = require('http');
+// var server = http.Server(app);
 
-app.use(express.static('client'));
+// app.use(express.static('client'));
 
-server.listen(PORT, function(){
-  console.log('Listening bitches');
-})
+// server.listen(PORT, function(){
+//   console.log('Listening bitches');
+// })
+// console.log(PORT)
+// var io = require('socket.io')(server);
 
-var io = require('socket.io')(server);
-
-io.on('connection', function(socket) {
-  socket.on('message', function(msg) {
-    io.emit('message', msg);
-  });
-});
+// io.on('connection', function(socket) {
+//   socket.on('message', function(msg) {
+//     io.emit('message', msg);
+//   });
+// });
