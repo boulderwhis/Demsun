b784b9f0a243b7f97aa9ae3a7bb2280fb12f2598
boulderwhis <eleavit1@kent.edu>
2020-06-06 20:52:09 -0700

    more index edits and back end framework addition

--- index.js
+++ index.js
@@ -6,21 +6,31 @@ const PORT = process.env.PORT || 5000
 express()
   .use(express.static(path.join('The-Democratic-Sun', 'public')))
   .set('views', path.join('The-Democratic-Sun', 'views'))
-  .set('view engine', 'ejs')
+  .set('view engine', 'ejs') //html
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-  var http = require('http'),
-    fs = require('fs');
+// IT"S LOOKING FOR A PAGES/INDEX FOLDER. CREATE PAGES FOLDER
 
 
-fs.readFile('./Democratic_Sun.html', function (err, html) {
-    if (err) {
-        throw err; 
-    }       
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(PORT);
-});
+
+
+
+
+
+
+
+//   var http = require('http'),
+//     fs = require('fs');
+
+
+// fs.readFile('./Democratic_Sun.html', function (err, html) {
+//     if (err) {
+//         throw err; 
+//     }       
+//     http.createServer(function(request, response) {  
+//         response.writeHeader(200, {"Content-Type": "text/html"});  
+//         response.write(html);  
+//         response.end();  
+//     }).listen(PORT);
+// });
