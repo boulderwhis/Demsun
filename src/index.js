5b62d0e524faf1d94a81218ee071e7b1c36852a3
boulderwhis <eleavit1@kent.edu>
2020-06-10 21:58:16 -0700

    Node lesson index file and dependencies deleted

---
+++ src/index.js
@@ -0,0 +1,30 @@
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const PORT = process.env.PORT || 5000
+
+const app = express();
+
+//database entrypoint
+const ads =[
+  {title: 'The sun is rising'}
+]
+
+app.use(helmet());
+
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.use(morgan('combined'));
+
+app.get('/', (req,res)=> {
+  res.send(ads);
+})
+
+app.listen(PORT, ()=>{
+  console.log('listening on port '+PORT)
+});
