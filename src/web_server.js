d8c53e60a1a8091cb0a56a0807cd078e5d547d5f
boulderwhis <eleavit1@kent.edu>
2020-06-11 20:11:01 -0700

    fs and unquote abs_path_2

--- src/web_server.js
+++ src/web_server.js
@@ -23,6 +23,7 @@
 
 
 const express = require('express');
+const fs = require('fs');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const helmet = require('helmet');
@@ -33,7 +34,7 @@ const PORT = process.env.PORT || 5000
 
 const app = express();
 
-var abs_path_2='The-Democratic-Sun'+'/tubular/';
+var abs_path_2=The-Democratic-Sun+'/tubular/';
 
 // console.log("\nRoot directory: "+ _dirname+"\n");
 // console.log("\nabs_path_2: "+abs_path_2+"\n");
